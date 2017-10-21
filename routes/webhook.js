a4c89c09a689a8562d21a4c7aebb0b9ef320c4e1
Abhilash K <abhilash1in@gmail.com>
2017-10-21 21:38:12 +0530

    pm2 describe

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,27 +1,41 @@
 var express = require('express');
 var router = express.Router();
 var crypto = require('crypto');
-
+var pm2 = require('pm2');
 const key = "route-test";
+const PID = "0";
 
-function verifyGitHub(req){
-  if (!req.headers['user-agent'].includes('GitHub-Hookshot')) {
-    return false;
-  }
-  // Compare their hmac signature to our hmac signature
-  // (hmac = hash-based message authentication code)
-  const theirSignature = req.headers['x-hub-signature'];
-  const payload = JSON.stringify(req.body);
-  const secret = 'route-test-123';
-  const ourSignature = `sha1=${crypto.createHmac('sha1', secret).update(payload).digest('hex')}`;
-  return crypto.timingSafeEqual(Buffer.from(theirSignature), Buffer.from(ourSignature));
+
+function verifyGitHub(req, secret){
+	if (!req.headers['user-agent'].includes('GitHub-Hookshot')) {
+		return false;
+	}
+// Compare their hmac signature to our hmac signature
+// (hmac = hash-based message authentication code)
+const theirSignature = req.headers['x-hub-signature'];
+const payload = JSON.stringify(req.body);
+const ourSignature = `sha1=${crypto.createHmac('sha1', secret).update(payload).digest('hex')}`;
+return crypto.timingSafeEqual(Buffer.from(theirSignature), Buffer.from(ourSignature));
 };
 
-/* GET home page. */
 router.post('/', function(req, res) {
-  //console.log(req.body);
-  console.log(verifyGitHub(req));
-  res.send("Thanks!");
+	console.log(verifyGitHub(req,"route-test"));
+	pm2.connect(function(err) {
+		if (err) {
+			console.error(err);
+			//process.exit(2);
+		}
+		pm2.describe(PID,function(pm2process,errback){
+			console.log(pm2process);
+		});
+		/*pm2.restart(PID, function(restartErr, apps) {
+			pm2.disconnect();   // Disconnects from PM2
+			if (restartErr){
+				console.error(restartErr);
+			}
+		});*/
+	});
+	res.send("Thanks!");
 });
 
 module.exports = router;
