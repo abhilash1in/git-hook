bfc55d58a8e7de8da23031428cd569ff544100ec
Ubuntu <ubuntu@ip-172-31-61-241.ec2.internal>
2017-10-21 14:18:58 +0000

    added verification

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
+
+const key = "route-test";
+
+function verifyGitHub(req){
+  if (!req.headers['user-agent'].includes('GitHub-Hookshot')) {
+    return false;
+  }
+  // Compare their hmac signature to our hmac signature
+  // (hmac = hash-based message authentication code)
+  const theirSignature = req.headers['x-hub-signature'];
+  const payload = JSON.stringify(req.body);
+  const secret = 'route-test-123';
+  const ourSignature = `sha1=${crypto.createHmac('sha1', secret).update(payload).digest('hex')}`;
+  return crypto.timingSafeEqual(Buffer.from(theirSignature), Buffer.from(ourSignature));
+};
 
 /* GET home page. */
 router.post('/', function(req, res) {
-  console.log(req);
+  //console.log(req.body);
+  console.log(verifyGitHub(req));
   res.send("Thanks!");
 });
 
