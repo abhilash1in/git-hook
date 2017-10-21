bdce6fcd6918e72a8446043b198cf43407e2d517
Abhilash K <abhilash1in@gmail.com>
2017-10-21 22:11:41 +0530

    git pull in nodejs hook

--- routes/webhook.js
+++ routes/webhook.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 var crypto = require('crypto');
 var pm2 = require('pm2');
+
 const key = "route-test";
 const PID = "7";
+const DIR = "/home/ubuntu/route-webhook-test/"
 
+const git = require('simple-git')(DIR);
 
 function verifyGitHub(req, secret){
 	if (!req.headers['user-agent'].includes('GitHub-Hookshot')) {
@@ -24,31 +27,39 @@ router.post('/', function(req, res) {
 		res.send(401,"Wrong Secret Key");
 		return;
 	}
-	pm2.connect(function(err) {
-		if (err) {
-			console.error(err);
+	pm2.connect(function(connectErr) {
+		if (connectErr) {
+			console.error(connectErr);
 			pm2.disconnect();
 			res.send("Failed. Could not connect to PM2");
 			return;
 		}
-		pm2.describe(PID,function(errback,pm2process){
-			if(errback){
-				console.error(err);
+		pm2.describe(PID,function(describeErr,pm2process){
+			if(describeErr){
+				console.error(describeErr);
 				pm2.disconnect();
 				res.send("Failed. Could not describe process with PID "+PID);
 				return;
 			}
 			const status = pm2process[0]['pm2_env'].status;
 			if(status == "online" || status == "errored" ){
-				pm2.restart(PID, function(restartErr) {
-					if(restartErr){
-						console.error(err);
+				git.pull(functon(pullErr,data){
+					if(pullErr){
+						console.error(pullErr);
 						pm2.disconnect();
-						res.send("Failed. Could not restart process with PID "+PID);
+						res.send("Failed. Could not perform git pull for directory: "+DIR);
 						return;
 					}
-					pm2.disconnect()
-					res.send("Success");
+					pm2.restart(PID, function(restartErr) {
+						if(restartErr){
+							console.error(restartErr);
+							pm2.disconnect();
+							res.send("Failed. Could not restart process with PID "+PID);
+							return;
+						}
+						pm2.disconnect()
+						res.send("Success");
+					});
 				});
 			}
 		});
